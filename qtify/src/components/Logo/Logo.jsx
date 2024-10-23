46e85fa52d849d0d5e8a4ac291c584eff2a648bc
umashankar <umashankarp33@gmail.com>
2024-10-23 23:29:28 +0530

    add style change

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import LogoImage from '../../assests/logo.png';
 
 export default function Logo() {
-  return <img src={LogoImage} alt='logo' width={67} />;
+  return (
+    <img
+      src={LogoImage}
+      alt='logo'
+      width={67}
+      height={34}
+      style={{ position: 'relative', left: '32px' }}
+    />
+  );
 }
